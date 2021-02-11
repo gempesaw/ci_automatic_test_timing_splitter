454f5c23fdfc033f9adda9c2e22dabeabaa99127
Daniel Gempesaw <gempesaw@gmail.com>
2021-02-11 18:28:21 +0000

    Implement optimal parallel finder

--- lib/ci_automatic_test_timing_splitter.ex
+++ lib/ci_automatic_test_timing_splitter.ex
@@ -2,10 +2,18 @@ import SweetXml
 
 defmodule CiAutomaticTestTimingSplitter do
   def main(args \\ []) do
-    args
-    |> parse_args
-    |> build_timings_hash
-    |> do_greedy_number_partitioning
+    {timings, count, index} =
+      args
+      |> parse_args
+      |> build_timings_hash
+
+    case count do
+      nil ->
+        determine_optimal_parallel({timings, 2, index})
+
+      _ ->
+        do_greedy_number_partitioning({timings, count, index})
+    end
     |> respond
   end
 
@@ -28,6 +36,7 @@ defmodule CiAutomaticTestTimingSplitter do
     %{timings_directory: d, machine_count: c, machine_index: i} =
       opts
       |> Enum.into(%{})
+      |> Map.put_new(:machine_count, nil)
       |> Map.put_new(:machine_index, nil)
 
     {d, c, i}
@@ -47,7 +56,7 @@ defmodule CiAutomaticTestTimingSplitter do
               }
             end).()
       end)
-      |> Enum.map(&elem(&1, 1))
+      |> Enum.map(fn {:ok, {timing, filename} } -> {timing |> Float.round(1), filename} end)
       |> Enum.sort_by(fn {timing, _} -> timing end, &>=/2)
 
     {timings, machine_count, machine_index}
@@ -58,52 +67,82 @@ defmodule CiAutomaticTestTimingSplitter do
       for index <- 1..machine_count,
           do: {index - 1, 0, []}
 
-    {partitioned_tests, _} =
+    partitioned_tests =
       timings
-      |> Enum.reduce(
-        {partitions, 0},
-        fn {timing, filename}, {partitions, smallest_index} ->
-          {index, duration, files} = Enum.at(partitions, smallest_index)
+      |> Enum.reduce(partitions, fn {timing, filename}, acc_partitions ->
+        {smallest_index, _} =
+          acc_partitions
+          |> Enum.reduce(fn {index, duration, _}, acc ->
+            {min_index, min_duration} = {elem(acc, 0), elem(acc, 1)}
 
-          new_partitions =
-            partitions
-            |> List.replace_at(
-              smallest_index,
-              {index, duration + timing, [{timing, filename} | files]}
-            )
+            case duration < min_duration do
+              true -> {index, duration}
+              false -> {min_index, min_duration}
+            end
+          end)
 
-          {new_smallest_index, _} =
-            new_partitions
-            |> Enum.reduce({smallest_index, duration + timing}, fn
-              {index, duration, _}, {min_index, min_duration} ->
-                case duration < min_duration do
-                  true -> {index, duration}
-                  false -> {min_index, min_duration}
-                end
-            end)
+        {index, duration, files} = Enum.at(acc_partitions, smallest_index)
 
-          {new_partitions, new_smallest_index}
-        end
-      )
+        acc_partitions
+        |> List.replace_at(
+          smallest_index,
+          {index, duration + timing, [{timing, filename} | files]}
+        )
+      end)
 
     {partitioned_tests, machine_index}
+  end
+
+  defp determine_optimal_parallel(
+         {timings, machine_count, machine_index},
+         {partitioned_tests, _} \\ {[], nil}
+       ) do
+    [{max_duration, _} | _] = timings
+    reversed = partitioned_tests |> Enum.reverse()
+
+    case reversed do
+      [] ->
+        IO.puts(:stderr, "the longest single test runs for #{max_duration |> Float.round(1)} seconds. trying with #{machine_count} groups...")
+
+        determine_optimal_parallel(
+          {timings, machine_count, machine_index},
+          do_greedy_number_partitioning({timings, machine_count, machine_index})
+        )
+
+      [{_, last_duration, _} | _] when last_duration > max_duration ->
+        IO.puts(:stderr, "other jobs duration: #{last_duration |> Float.round(1)}. trying again with #{machine_count + 1} groups...")
+
+        determine_optimal_parallel(
+          {timings, machine_count + 1, machine_index},
+          do_greedy_number_partitioning({timings, machine_count + 1, machine_index})
+        )
+
+      _ ->
+        IO.puts(:stderr, "optimal parallel found at #{machine_count} parallel jobs")
+        {partitioned_tests, machine_index}
+    end
   end
 
   defp respond({partitioned_tests, machine_index}) when is_integer(machine_index) do
     partitioned_tests
     |> Enum.find(fn {index, _, _} -> index == machine_index end)
     |> elem(2)
-    |> Enum.map(&elem(&1, 1))
-    |> IO.puts()
+    |> Enum.each(fn {_, filename} -> IO.puts(filename) end)
   end
 
   defp respond({partitioned_tests, _}) do
     partitioned_tests
     |> Enum.map(&Tuple.to_list(&1))
     |> Enum.map(fn [index, duration, files] ->
-      [index, duration, Enum.reduce(files, "", fn {duration, filename}, acc -> "#{duration} - #{filename}\n#{acc}" end)]
+      [
+        index,
+        duration |> Float.round(1),
+        Enum.reduce(files, "", fn {duration, filename}, acc ->
+          "#{duration} - #{filename}\n#{acc}"
+        end)
+      ]
     end)
     |> TableRex.quick_render!()
-    |> IO.puts
+    |> IO.puts()
   end
 end
