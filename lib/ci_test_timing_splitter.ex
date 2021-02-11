5609d5bebedc321dd762f2c42bd808ac9db810b8
Daniel Gempesaw <gempesaw@gmail.com>
2021-02-11 02:53:11 +0000

    Add results of `mix new _project_name_`

---
+++ lib/ci_test_timing_splitter.ex
@@ -0,0 +1,18 @@
+defmodule CiTestTimingSplitter do
+  @moduledoc """
+  Documentation for `CiTestTimingSplitter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CiTestTimingSplitter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ci_test_timing_splitter_test.exs
@@ -0,0 +1,8 @@
+defmodule CiTestTimingSplitterTest do
+  use ExUnit.Case
+  doctest CiTestTimingSplitter
+
+  test "greets the world" do
+    assert CiTestTimingSplitter.hello() == :world
+  end
+end
